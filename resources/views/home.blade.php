6b4e8672d755fc3b535193b8efaf09253c43b1df
Maciej Zienkiewicz <pxmaciej@outlook.com>
2020-05-21 23:33:07 +0200

    GameLogic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -13,12 +14,38 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                 
+               
                     <a type="button" class="btn btn-success" href="/game/{{Auth::user()->id}}">GRAJ</a>
-                  
+                   
+
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+@section('game')
+<div class="container">
+    <div class="row justify-content-center">
+        
+                <div class="col-md-5">jak sie nazwa wykonawca
+                </div>
+
+                <div class="col-md-2">nazywa sie:
+                    <div class="row">
+                        <div class="col">jurek
+                        </div>
+                        <div class="col">marek
+                        </div>
+                        <div class="col">barek
+                        </div>
+                    </div>   
+                </div>
+
+    </div>
+</div>
+@endsection
+
+
+
