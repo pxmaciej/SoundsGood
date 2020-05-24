96c769186668ab29c2bad1cb4e5018a712fe9746
Maciej Zienkiewicz <pxmaciej@outlook.com>
2020-05-24 22:57:56 +0200

    Game Logic Done

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -17,28 +17,43 @@ class GameController extends Controller
     public function index(int $id){
     
         $update='play';
-        $limit = 10;
-        $offset = 5;
+        $limit = 100;
+        $offset = 100;
         $api = new Larafy('8e7bdb2251274727bb96bc8100d8a5db', 'c33612abf7e2452d87581511be374880');
-       
+        $api->setMarket('PL')->setLocale('pl_PL');
         try {
-            $playlist = $api->getTrack('7EjyzZcbLxW7PaaLua9Ksb');
+           // O_25srbGRMuWjM4IAzyTQg
+           //5GEf0fJs9xBPr5R4jEQjtw
+          //37i9dQZEVXbN6itCcaL3Tt
+            $playlist = $api->getPlaylist('37i9dQZEVXbN6itCcaL3Tt', $limit, $offset);
+          
         } catch(\Rennokki\Larafy\Exceptions\SpotifyAPIException $e) {
             // invalid data sent
             $e->getAPIResponse(); // Get the JSON API response.
-        }
+        }   
+        //@dd($playlist);
+           $temp = $playlist->tracks->items;
+
+       
+        // @dd($temp);
         
-        $track = $playlist;
+           $tracks = collect($temp)->filter(function( $item){
+               return $item->track->preview_url != null;
+           })->shuffle()->take(3); 
+        
+        //@dd($tracks);
+
     
-
-
-  
       
+           
+           $current_track = $tracks->first();
+           $shuffled = $tracks->shuffle();
 
-          
+           
 
+     
         
-      return View::make('form',['track' => $track,'update'=> $update]);
+      return View::make('form',['track' => $current_track, 'tracks' => $shuffled,'update'=> $update]);
         
         
 
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -3,30 +3,32 @@
 @section('game')
 @if( $update  =='play')
 <main class="py-4">
-<div class="container">
-    <div class="row justify-content-center">
-        
-                <div class="col-md-5">
-         
-                    <audio autoplay id="song">
-                        <source src="{!! $track->preview_url !!}" type="audio/mp3">
-                    </audio>
-              
-                </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            
+                    <div class="col-md-5">
+            
+                        <audio autoplay id="song">
+                            <source src="{!! $track->track->preview_url !!}" type="audio/mp3">
+                        </audio>
+                
+                    </div>
 
-                <div class="col-md-2">nazywa sie:
-                    <div class="row">
-                        <div class="col">jurek
-                        </div>
-                        <div class="col">marek
-                        </div>
-                        <div class="col">barek
-                        </div>
-                    </div>   
-                </div>
+                    <div class="col-md-2">nazywa sie:
+                            
+                            @foreach($tracks as $track)
+                            <div class="row">
+                                <button class="btn btn-success"  name="answer"  type="submit" value="{{ $track->track->id }}" href="{{}}">
+                                    {{ $track->track->name}}
+                                </button>
+                            </div>
 
+                            @endforeach
+                         
+                    </div>
+
+        </div>
     </div>
-</div>
 </main>
 @endif 
 @endsection
