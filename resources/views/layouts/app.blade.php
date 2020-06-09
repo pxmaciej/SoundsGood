8e04ec39e267236afc237c79600b5210f98c7a62
Maciej Zienkiewicz <pxmaciej@outlook.com>
2020-06-09 11:51:49 +0200

    Auth do routs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'SoundsGood') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('lang/{locale}', function ($locale){
     Session::put('locale', $locale);
     return redirect()->back();
 });
-Route::get('/game/{points}/{wrong}/{temp}', 'GameController@index');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/points/{song_id}/{current_track}/{points}/{wrong}/{temp}', 'GamePointsController@index');
+Route::get('/game/{points}/{wrong}/{temp}', 'GameController@index')->middleware('auth');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+Route::get('/points/{song_id}/{current_track}/{points}/{wrong}/{temp}', 'GamePointsController@index')->middleware('auth');
 Route::get('/leaderboard','LeaderBoardController@index');
