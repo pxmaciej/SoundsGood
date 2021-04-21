39fc4e36fae38e47eb6c4e8c780d473629f442af
pxmaciej <56204020+pxmaciej@users.noreply.github.com>
2021-04-21 22:21:30 +0200

    ddos

--- config/app.php
+++ config/app.php
@@ -162,11 +162,11 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-      
+
         /*
          * Package Service Providers...
          */
-        
+
         /*
          * Application Service Providers...
          */
@@ -175,7 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-       
+
 
     ],
 
@@ -227,8 +227,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Throttle' => GrahamCampbell\Throttle\Facades\Throttle::class,
 
     ],
-  
+
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,22 @@
 use App\Http\Controllers\LeaderBoardController;
 use App\Leaderboard;
 //use Illuminate\Routing\Route;
-use Illuminate\Support\Facades\Route; 
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'HomeController@index');
-Auth::routes();
-Route::get('lang/{locale}', function ($locale){
-    Session::put('locale', $locale);
-    return redirect()->back();
-});
-Route::get('/game/{points}/{wrong}/{temp}', 'GameController@index')->middleware('auth');
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
-Route::get('/points/{song_id}/{current_track}/{points}/{wrong}/{temp}', 'GamePointsController@index')->middleware('auth');
-Route::get('/leaderboard','LeaderBoardController@index');
+
+
+
+
+  Route::group(['middleware' => 'throttle:60,1'], function () {
+        Route::get('/', 'HomeController@index');
+        Auth::routes();
+        Route::get('lang/{locale}', function ($locale){
+            Session::put('locale', $locale);
+            return redirect()->back();
+        });
+        Route::get('/game/{points}/{wrong}/{temp}', 'GameController@index')->middleware('auth');
+        Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+        Route::get('/points/{song_id}/{current_track}/{points}/{wrong}/{temp}', 'GamePointsController@index')->middleware('auth');
+        Route::get('/leaderboard','LeaderBoardController@index');
+
+  });
